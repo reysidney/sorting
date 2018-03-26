7685da45d02a067df47c605ba374ec9faedb4316
Rey Sidney Carredo <rscarredo@gmail.com>
2018-03-26 13:14:50 +0800

    update

--- sort.js
+++ sort.js
@@ -13,7 +13,7 @@ Array.prototype.pop = function (index) {
     return result;
 }
 
-Array.prototype.heapSort = function () {
+Array.prototype.Sort = function () {
 
     function findMaxIndex(a) {
         var max = 0;
@@ -27,7 +27,7 @@ Array.prototype.heapSort = function () {
         return maxIndex;
     }
 
-    function heapSort (a) {
+    function Sort (a) {
         var maxIndex;
         var len = a.length;
         var lenIndex;
@@ -42,9 +42,51 @@ Array.prototype.heapSort = function () {
         }
     }
 
-    heapSort(this);
+    Sort(this);
     return this;
 };
+
+// reference: https://www.w3resource.com/javascript-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-3.php
+Array.prototype.heapSort = function () {
+    function heap_root(input, i) {
+        var left = 2 * i + 1;
+        var right = 2 * i + 2;
+        var max = i;
+    
+        if (left < array_length && input[left] > input[max]) {
+            max = left;
+        }
+    
+        if (right < array_length && input[right] > input[max])     {
+            max = right;
+        }
+    
+        if (max != i) {
+            input.swap( i, max);
+            heap_root(input, max);
+        }
+    }
+
+    function heapSort(input) {
+    
+        array_length = input.length;
+    
+        for (var i = Math.floor(array_length / 2); i >= 0; i -= 1)      {
+            heap_root(input, i);
+          }
+    
+        for (i = input.length - 1; i > 0; i--) {
+            input.swap(0, i);
+            array_length--;
+          
+          
+            heap_root(input, 0);
+        }
+    }
+
+    heapSort(this);
+    return this;
+}
 
 //reference: https://www.nczonline.net/blog/2012/11/27/computer-science-in-javascript-quicksort/
 Array.prototype.quickSort = function () {
